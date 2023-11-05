8501e8c23bebf740215fd3a46ef0f3a5e7ef6c13
NEBULA1978 <rpascualey@gmail.com>
2023-11-05 21:06:31 +0100

    Añadiendo cerpetas separando archivos primeros

---
+++ pruebas-grid-primeras/arrays.js
@@ -0,0 +1,2 @@
+imagenes=["3_macarrones.png","chupachups.png","gorra.png","lapiz.png","maquineta.png","mechero.png","palillos.png","peine.png","pinza.png","tenedor_lapiz.png","marco_foto.png","llavero.png"];
+descripciones=["Macarrones ecológicos","Chupachups sin chupar","Gorra de la campaña de Vic del 2002","Lapiz de grafito magnético","Maquineta a pilas","Mechero eléctrico","Palillos amiantados","Peine regalo","Pinza (por si se te va la principal)","Tenedor-lápiz muy práctico","Marco de foto retro","Llavero anti-balas"];
